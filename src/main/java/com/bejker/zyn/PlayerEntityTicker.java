c66fbdec72b321b686a5ab5e65d42152a28aa1ff
bejker123 <1piotrkucharski@gmail.com>
2025-01-12 19:53:10 +0100

    feat: effects

--- src/main/java/com/bejker/zyn/PlayerEntityTicker.java
+++ src/main/java/com/bejker/zyn/PlayerEntityTicker.java
@@ -1,21 +1,29 @@
 package com.bejker.zyn;
 
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
 
 import static com.bejker.zyn.items.ZynItem.NICOTINE_CONTENT;
 
 //TODO: implement for other entities
 public class PlayerEntityTicker {
 
+    //Max concentration threshold in nmol/L
+    private static final float MAX_CONC = 2*444.f;
+    //Min concentration threshold in nmol/L
+    private static final float MIN_CONC =  25.f;
 
     //In L
     private static final float BLOOD_VOLUME = 5.0f;
 
-    //TODO: find the right factor
+    //TODO: Check balancing
     //In nmol
-    private static final float SCALING_FACTOR = 0.01f;
+    private static final float SCALING_FACTOR = 0.0148f;
 
     //In nmol/L
     private static float calculate_nicotine_concentration(int content_){
@@ -32,7 +40,32 @@ public class PlayerEntityTicker {
         }
         player.setAttached(NICOTINE_CONTENT,attached);
         float concentration = calculate_nicotine_concentration(attached);
-        player.sendMessage(Text.literal(String.format("%.3f",concentration)+" nmol/L"),true);
+        int interval = (int) Math.floor(concentration / MIN_CONC);
+        player.sendMessage(Text.literal(String.format("%.3f",concentration)+" nmol/L; "+attached+" interval: "+interval).formatted(Formatting.GOLD),true);
+        if(interval >= 1){
+            if(!player.hasStatusEffect(StatusEffects.HASTE)){
+                player.addStatusEffect(new StatusEffectInstance(StatusEffects.HASTE,400,1),player);
+            }
+            if(interval >= 2){
+                if(!player.hasStatusEffect(StatusEffects.SPEED)) {
+                    player.addStatusEffect(new StatusEffectInstance(StatusEffects.SPEED, 400, 1), player);
+                }
+                if(interval >= 3){
+                    if(!player.hasStatusEffect(StatusEffects.STRENGTH)) {
+                        player.addStatusEffect(new StatusEffectInstance(StatusEffects.STRENGTH, 400, 1), player);
+                    }
+                    if(interval >= 17){
+                        if(!player.hasStatusEffect(StatusEffects.NAUSEA)) {
+                            player.addStatusEffect(new StatusEffectInstance(StatusEffects.NAUSEA, 400, 2), player);
+                        }
+                        if(concentration >= MAX_CONC){
+                            player.setAttached(NICOTINE_CONTENT,0);
+                            player.kill((ServerWorld) player.getWorld());
+                        }
+                    }
+                }
+            }
+        }
     }
     public static void tick(PlayerEntity player_){
         if(player_ instanceof ServerPlayerEntity player){
