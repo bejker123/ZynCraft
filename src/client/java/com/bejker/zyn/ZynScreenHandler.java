68f84200a813df0210f022cfb61abaff9e6e8e0d
bejker123 <1piotrkucharski@gmail.com>
2025-01-12 18:21:24 +0100

    ref: clean up code

--- src/client/java/com/bejker/zyn/ZynScreenHandler.java
+++ src/client/java/com/bejker/zyn/ZynScreenHandler.java
@@ -1,27 +1,14 @@
 package com.bejker.zyn;
 
-import com.bejker.zyn.inventory.ZynSlot;
-import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.client.render.GameRenderer;
 import net.minecraft.client.render.RenderLayer;
-import net.minecraft.client.render.RenderLayers;
-import net.minecraft.entity.boss.BossBar;
 import net.minecraft.screen.slot.Slot;
-import net.minecraft.util.Colors;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.math.ColorHelper;
-
-import java.awt.*;
-
-import static com.bejker.zyn.inventory.ZynInventory.SLOT_X;
-import static com.bejker.zyn.inventory.ZynInventory.SLOT_Y;
 
 public class ZynScreenHandler {
     private static final Identifier SLOT_TEXTURE = ZynCraft.id("textures/gui/zyn_slot.png");
-
     private static final Identifier SLOT_HIGHLIGHT_BACK_TEXTURE = Identifier.ofVanilla("container/slot_highlight_back");
     private static final Identifier SLOT_HIGHLIGHT_FRONT_TEXTURE = Identifier.ofVanilla("container/slot_highlight_front");
 
@@ -30,12 +17,9 @@ public class ZynScreenHandler {
         assert screen != null;
         int x = -20;
         int y = 5;
-        //RenderSystem.setShader(MinecraftClient.getInstance().gameRenderer.);
         context.getMatrices().push();
         context.getMatrices().translate(x, y, 1.0f);
-        context.drawTexture(i->{
-            return RenderLayer.getGuiTextured(i);
-        },SLOT_TEXTURE,0,0,0,0,20,20,20,20);
+        context.drawTexture(RenderLayer::getGuiTextured,SLOT_TEXTURE,0,0,0,0,20,20,20,20);
         if(focused_slot != null&&focused_slot.id == ZynCraft.ZYN_SLOT){
             x = -2;
             y = -2;
